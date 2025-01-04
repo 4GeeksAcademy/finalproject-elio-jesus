4732becdda6e946b83d3b5ba113730d7856ab2bd
yesus011 <jesu8907@gmail.com>
2025-01-04 02:03:56 +0000

    registerYbotonesdeNavbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,12 +6,19 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Gym Bro</span>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				<div className="d-flex justify-content-end ">
+					<div className="ml-auto">
+						<Link to="/demo">
+							<button className="btn btn-primary me-2">Login</button>
+						</Link>
+					</div>
+					<div className="ml-auto">
+						<Link to="/register">
+							<button className="btn btn-primary">Register</button>
+						</Link>
+					</div>
 				</div>
 			</div>
 		</nav>
