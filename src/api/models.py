a042a5cfe592dbab1d57a91f693e38d875d5e63a
elioaca01 <eioaca.06@gmail.com>
2025-01-04 00:26:23 +0000

    back-end register

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,16 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(40), nullable=False)
+    lastName = db.Column(db.String(50), nullable=False)
+    birthDate = db.Column(db.Date())
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(20), unique=True,nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
+    salt = db.Column(db.String(180), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False, default=False)
+    create_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), nullable=False)
+    update_at = db.Column(db.DateTime(timezone=True), default=db.func.now(),onupdate=db.func.now(), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
